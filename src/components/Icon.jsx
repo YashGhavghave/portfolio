a91ef14d7a19a4890b06a01e997e1b0a8d39066a
Yash_G.G <157970758+YashGhavghave@users.noreply.github.com>
2024-11-21 17:51:31 +0530

    hyper reference

--- src/components/Icon.jsx
+++ src/components/Icon.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Icon() {
   return (
-    <div className='font-bold text-green-400 fixed top-[5vh] left-[5vw] text-5xl bg-black'>
+    <div href="" className='font-bold text-green-400 fixed top-[5vh] left-[5vw] text-5xl bg-black'>
       YASH.G
     </div>
   )
